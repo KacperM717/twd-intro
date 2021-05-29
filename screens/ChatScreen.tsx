019787d8d45b20bc10967b19cdd48cbb1fdfade0
KacperM717 <kacpermikolajczak10@gmail.com>
2021-05-29 09:39:08 +0200

    Chat Screen styled.

--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,7 +1,15 @@
 import { useMutation, useQuery, useSubscription } from "@apollo/client";
 import { StackScreenProps } from "@react-navigation/stack";
 import React, { useState } from "react";
-import { GiftedChat, IMessage } from "react-native-gifted-chat";
+import { TextInputComponent } from "react-native";
+import {
+  Bubble,
+  Composer,
+  GiftedChat,
+  GiftedChatProps,
+  IMessage,
+  InputToolbar,
+} from "react-native-gifted-chat";
 import { client } from "../api";
 import { SEND_MESSAGE, SEND_USER_TYPING } from "../api/mutation";
 import { GET_ROOM, GET_USER } from "../api/query";
@@ -9,9 +17,13 @@ import {
   SUBSCRIBE_ROOM_MESSAGE,
   SUBSCRIBE_ROOM_TYPING,
 } from "../api/subscription";
+import SvgIcon from "../components/Icon";
 import Loader from "../components/Loader";
+import { View, Text } from "../components/Themed";
 import { AuthStackParams, Message } from "../types";
 import giftedMapper from "../utils/giftedChatMapper";
+
+import SendIcon from "../assets/images/svg/send.svg";
 
 export function ChatScreen({
   navigation,
@@ -71,6 +83,92 @@ export function ChatScreen({
       onInputTextChanged={handleTyping}
       alwaysShowSend={true}
       inverted={false}
+      renderBubble={renderBubble}
+      renderChatFooter={renderFooter}
+      renderInputToolbar={renderToolbar}
+      renderComposer={renderComposer}
+    />
+  );
+}
+
+function renderBubble(props: GiftedChatProps) {
+  // const both = {
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   alignItems: "flex-start",
+  //   padding: 12,
+  //   marginBottom: 16,
+  //   width: "60%",
+  // };
+
+  return (
+    <Bubble
+      {...props}
+      renderTime={() => null}
+      wrapperStyle={{
+        left: {
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          padding: 8,
+          marginBottom: 8,
+
+          backgroundColor: "#FFFFFF",
+        },
+        right: {
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "flex-start",
+          padding: 8,
+          marginBottom: 8,
+
+          backgroundColor: "#993AFC",
+        },
+      }}
+    />
+  );
+}
+
+function renderFooter(props: GiftedChatProps) {
+  return (
+    <View {...props}>
+      <Text>TODO: isTyping</Text>
+    </View>
+  );
+}
+
+function renderToolbar(props: GiftedChatProps) {
+  return (
+    <InputToolbar
+      {...props}
+      containerStyle={{
+        backgroundColor: "#B6DEFD",
+        paddingTop: 15,
+        paddingBottom: 12,
+        paddingHorizontal: 16,
+        borderTopLeftRadius: 12,
+        borderTopRightRadius: 12,
+      }}
+      renderActions={(props) => null}
+      renderSend={(props) => <SvgIcon xml={SendIcon} />}
+    />
+  );
+}
+
+function renderComposer(props: GiftedChatProps) {
+  return (
+    <Composer
+      {...props}
+      textInputStyle={{
+        backgroundColor: "#FFFFFF",
+        color: "#000000",
+        borderRadius: 12,
+        borderBottomRightRadius: 0,
+        padding: 12,
+        borderBottomWidth: 0,
+      }}
     />
   );
 }
